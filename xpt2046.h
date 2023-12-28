1d0c703e7abc2df068f12cf0a746c027b75b18c7
ngc6589 <z-gypsy@nifty.com>
2023-12-29 00:33:00 +0900

    change C++ compilation compatible

--- xpt2046.h
+++ xpt2046.h
@@ -2,6 +2,10 @@
 #define XPT2046_H
 #include "lvgl.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #define TP_PORTRAIT 0
 #define TP_LANDSCAPE 1
 #define TP_INV_PORTRAIT 2
@@ -12,4 +16,7 @@ int xpt2046_raw_pos(uint16_t * x, uint16_t * y);
 int xpt2046_Pos(uint16_t * posX, uint16_t * posY);
 void xpt2046_read_cb(lv_indev_t * indev, lv_indev_data_t * data);
 
+#ifdef __cplusplus
+}
 #endif
+#endif // XPT2046_H
